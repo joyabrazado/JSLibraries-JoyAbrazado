1fc5b36175debbcd3b387d50c229aa395bc12514
joyabrazado <mbabrazado@myseneca.ca>
2024-07-18 11:36:31 -0400

    content + animation

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,27 @@
 // CAROUSEL
 document.addEventListener('DOMContentLoaded', function () {
-  new Glide('.glide', {
-    type: 'carousel',
-    startAt: 0,
-    perView: 1,
-    autoplay: 3000
-  }).mount();
+    new Glide('.glide', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 1,
+        autoplay: 3000
+    }).mount();
+});
+
+// ANIMEJS
+anime.timeline({
+    easing: 'easeOutExpo', 
+})
+.add({
+    targets: '#content h2', 
+    opacity: [0, 1],
+    translateY: [-50, 0], 
+    duration: 800
+})
+.add({
+    targets: '#content p', 
+    opacity: [0, 1],
+    translateY: [50, 0], 
+    duration: 800,
+    offset: '-=400' 
 });
