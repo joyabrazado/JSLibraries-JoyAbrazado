88ca1b29213793efa8156f42391e5ba0fb4014cb
joyabrazado <mbabrazado@myseneca.ca>
2024-07-18 12:45:16 -0400

    header and footer gradient animation

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -10,18 +10,53 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // ANIMEJS
 anime.timeline({
-    easing: 'easeOutExpo', 
+    easing: 'easeOutExpo',
 })
-.add({
-    targets: '#content h2', 
-    opacity: [0, 1],
-    translateY: [-50, 0], 
-    duration: 800
-})
-.add({
-    targets: '#content p', 
-    opacity: [0, 1],
-    translateY: [50, 0], 
-    duration: 800,
-    offset: '-=400' 
+    .add({
+        targets: '#content h2',
+        opacity: [0, 1],
+        translateY: [-50, 0],
+        duration: 800
+    })
+    .add({
+        targets: '#content p',
+        opacity: [0, 1],
+        translateY: [50, 0],
+        duration: 800,
+        offset: '-=400'
+    });
+
+// GRANIMJS
+var headerGradient = new Granim({
+    element: '#headerCanvas',
+    direction: 'left-right', 
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    states: {
+        "default-state": {
+            gradients: [
+                ['#FFC0CB', '#FFB6C1'],
+                ['#FFDAB9', '#F4C2C2'],
+                ['#DCAE96', '#FFF5EE']
+            ],
+            transitionSpeed: 15000
+        }
+    }
+});
+
+var footerGradient = new Granim({
+    element: '#footerCanvas',
+    direction: 'left-right', 
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    states: {
+        "default-state": {
+            gradients: [
+                ['#FFC0CB', '#FFB6C1'],
+                ['#FFDAB9', '#F4C2C2'],
+                ['#DCAE96', '#FFF5EE']
+            ],
+            transitionSpeed: 15000
+        }
+    }
 });
